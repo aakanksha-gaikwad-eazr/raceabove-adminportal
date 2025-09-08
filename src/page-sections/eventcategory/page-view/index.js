a93061ca8f0a9a1f3229b7904c6f5c70db131d18
aakanksha-gaikwad-eazr <aakanksha.g@geteazr.com>
2025-09-08 17:02:21 +0530

    Update index.js

--- src/page-sections/eventcategory/page-view/index.js
+++ src/page-sections/eventcategory/page-view/index.js
@@ -3,4 +3,4 @@ export { default as EventCategoryList2PageView } from './eventcategory-list-2';
 export { default as EventCategoryGrid1PageView } from './eventcategory-grid-1';
 export { default as EventCategoryGrid2PageView } from './eventcategory-grid-2';
 export { default as AddNewEventCategoryPageView } from './add-new-eventcategory';
-export { default as EditEventCategoryFormModal } from './EditeventcategoryFormModal';
+export { default as EditEventCategoryFormModal } from './editeventcategoryFormModal';
